5235e87c84b9ec199b672e1cbbb7213fb11298be
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-16 11:10:02 +0530

    Added: code to validate 6 digit pincode

--- src/com/regex/PinCodeRegex.java
+++ src/com/regex/PinCodeRegex.java
@@ -1,10 +1,15 @@
 package com.regex;
 
+import java.util.regex.Pattern;
+
 public class PinCodeRegex {
 
 	public static void main(String[] args) {
       
 		System.out.println("Welcome to Java regex program");
+		String postalPattern = "567895"; //string to be validated
+		String matcher = "[1-9]{1}[0-9]{5}"; //regular expression
+		System.out.println(Pattern.matches(matcher,postalPattern));
 	}
 
 }
