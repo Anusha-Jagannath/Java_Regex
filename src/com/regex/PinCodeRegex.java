f119d74ade2475be25e89446a5aa4327479d08db
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-16 11:20:03 +0530

    Added: code to restrict character at beginning

--- src/com/regex/PinCodeRegex.java
+++ src/com/regex/PinCodeRegex.java
@@ -7,7 +7,7 @@ public class PinCodeRegex {
 	public static void main(String[] args) {
       
 		System.out.println("Welcome to Java regex program");
-		String postalPattern = "567895"; //string to be validated
+		String postalPattern = "A67895"; //string to be validated
 		String matcher = "[1-9]{1}[0-9]{5}"; //regular expression
 		System.out.println(Pattern.matches(matcher,postalPattern));
 	}
