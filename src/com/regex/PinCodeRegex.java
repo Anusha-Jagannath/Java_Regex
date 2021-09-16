d60f3890f7590588b2c19cc8c54452713023a4a6
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-16 11:25:33 +0530

    Added: code to restrict characters at end

--- src/com/regex/PinCodeRegex.java
+++ src/com/regex/PinCodeRegex.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 public class PinCodeRegex {
 
 	public static void main(String[] args) {
-      
+
 		System.out.println("Welcome to Java regex program");
-		String postalPattern = "A67895"; //string to be validated
-		String matcher = "[1-9]{1}[0-9]{5}"; //regular expression
-		System.out.println(Pattern.matches(matcher,postalPattern));
+		String postalPattern = "400088B"; // string to be validated
+		String matcher = "[1-9]{1}[0-9]{5}$"; // regular expression to restrict character at end and begining
+		System.out.println(Pattern.matches(matcher, postalPattern));//prints false
 	}
 
 }
