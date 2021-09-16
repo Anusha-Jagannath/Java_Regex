6f1f48ff7191bb3a3aacbef159f8c0f880e446ee
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-16 11:33:00 +0530

    Added: code to validate space in pincode

--- src/com/regex/PinCodeRegex.java
+++ src/com/regex/PinCodeRegex.java
@@ -7,9 +7,9 @@ public class PinCodeRegex {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Java regex program");
-		String postalPattern = "400088B"; // string to be validated
-		String matcher = "[1-9]{1}[0-9]{5}$"; // regular expression to restrict character at end and begining
-		System.out.println(Pattern.matches(matcher, postalPattern));//prints false
+		String postalPattern = "400 088"; // string to be validated
+		String matcher = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$"; // regular expression to restrict character at end, beginning,space validity
+		System.out.println(Pattern.matches(matcher, postalPattern));//prints true for 400 088 and 400088
 	}
 
 }
